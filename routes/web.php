a7c7765a19c91e0b1d312dcaedd349079e6b29b7
holysabre <1026579245@qq.com>
2019-06-17 11:34:59 +0800

    编辑个人资料

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ Route::get('/', 'PagesController@root')->name('root');
 
 Auth::routes();
 
-Route::resource('users','UsersController',['only'=>['show','update','edit']]);
+Route::resource('users','UsersController',['only'=>['show','edit','update']]);
