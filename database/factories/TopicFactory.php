d97d09d58ec31e9fb290b9e044323bd27038d40c
holysabre <1026579245@qq.com>
2019-06-18 10:02:47 +0800

    填充用户和话题数据

--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -3,7 +3,21 @@
 use Faker\Generator as Faker;
 
 $factory->define(App\Models\Topic::class, function (Faker $faker) {
+
+    //随机生成小段落
+    $sentence = $faker->sentence;
+
+    //随机取当前月的时间
+    $updated_at = $faker->dateTimeThisMonth();
+
+    //随机取当前月的时间 但要比更新时间早
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'created_at' => $created_at,
+        'updated_at' => $updated_at,
     ];
 });
