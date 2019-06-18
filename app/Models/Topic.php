4f3e72d06d08dbbacabc2ef82949aa15fa54b484
holysabre <1026579245@qq.com>
2019-06-18 14:58:35 +0800

    话题排序

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -15,4 +15,49 @@ class Topic extends Model
     {
         return $this->belongsTo(User::class);
     }
+
+    /**
+     * @param $query
+     * @param $order
+     * @return mixed
+     * 根据排序条件返回数据
+     * 利用scope本地域
+     */
+    public function scopeWithOrder($query, $order)
+    {
+        switch ($order)
+        {
+            case 'recent':
+                $query->recent($query);
+                break;
+
+            default:
+                $query->recentReplied($query);
+                break;
+        }
+        //预防N+1问题
+        return $query->with('user','category');
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     * 按修改时间排序
+     * 当有回复时，会更新修改时间
+     */
+    public function scopeRecentReplied($query)
+    {
+        return $query->orderBy('updated_at', 'desc');
+    }
+
+    /**
+     * @param $query
+     * @return mixed
+     * 按创建时间排序
+     */
+    public function scopeRecent($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
+
 }
