c55efed4e7b2538ef1c28b09dc984569d7fd95a5
holysabre <1026579245@qq.com>
2019-06-18 15:33:48 +0800

    用户可以创建话题

--- app/helpers.php
+++ app/helpers.php
@@ -1,11 +1,32 @@
 <?php
 
+/**
+ * @return mixed
+ * 替换路由样式
+ */
 function route_class()
 {
     return str_replace('.', '-', Route::currentRouteName());
 }
 
+/**
+ * @param $category_id
+ * @return string
+ * 分类的选中状态
+ */
 function category_nav_active($category_id)
 {
     return active_class((if_route('categories.show') && if_route_param('category', $category_id)));
+}
+
+/**
+ * @param $value
+ * @param int $length
+ * @return string
+ * 截取字符
+ */
+function make_excerpt($value, $length = 200)
+{
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
 }
